6e919e6fe338217e5e0a2c17faa9175434d877e8
rogerboroda <roger_boroda@mail.ru>
2024-08-30 21:07:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+homework = 12
+time_consuming = 1.5
+name_of_the_course = 'Python'
+average_run_time = time_consuming / homework
+print(name_of_the_course,',''всего задач' ':',homework,',''затрачено часов'':',time_consuming,',''среднее время выполнения'':',average_run_time)
